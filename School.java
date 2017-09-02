9ab3643aec3828f02ac2f8ea7ec1510d16ba9fe4
gjoshua <31380625+gjoshua@users.noreply.github.com>
2017-09-02 21:50:55 +0530

    Add files via upload
    Interface

---
+++ School.java
@@ -0,0 +1,57 @@
+public interface information
+  {
+    void details();
+	void address();
+  }
+  
+public class Student implements information
+   {
+     public void details()
+	   {
+	     System.out.println("Student details");
+	   }
+	public void addresds()
+       {
+	    System.out.println("Student address");
+	   }	
+    } 
+	
+public class Teacher implements information
+   {
+     public void details()
+	   {
+	     System.out.println("Teacher details");
+	   }
+	public void addresds()
+       {
+	    System.out.println("Teacher address");
+	   }	
+    } 
+	
+public class School
+  {
+    public static void main(String[] args)
+	   {
+	     Student s1=new Student();
+		    s1.details();
+			s1.address();
+		 
+         Teacher t1=new Teacher();
+            t1.details();
+            t1.address();
+        }
+   }		
+	
+		
+
+
+
+
+
+
+
+
+
+
+
+
